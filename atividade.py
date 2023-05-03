a3bf5c20e1981166a66abf7055a66c82be890560
daltonfranco <daltonpacolafranco.git@hotmail.com>
2023-05-03 00:56:22 +0000

    alterações completas

--- atividade.py
+++ atividade.py
@@ -13,8 +13,10 @@ quantidade = []
 lista_menores_valores = []
 lista_maiores_valores = []
 numeros = []
+numeros1 = []
 
 texto_limpo = False
+funcao = False
 
 pergunta_global = 1
 
@@ -22,8 +24,6 @@ while pergunta_global != '0':
 
 
     pergunta_p = input('''
-
-                
                 [1] - limpar os acentos desse texto
                 [2] - Realizar a contagem das palavras e mostrar a quantidade das palavras em ordem decrescente
                 [3] - Mostrar a quantidade de alguma palavra
@@ -31,7 +31,7 @@ while pergunta_global != '0':
                 [5] - Mostrar as palavras que mais apareceram no texto
                 [0] - Sair do programa
 
-        Qual você quer --> '''
+        Qual opção você deseja? --> '''
     )
 
 
@@ -42,20 +42,23 @@ while pergunta_global != '0':
 
     elif pergunta_p == '1':
 
-        conteudo_limpo = unidecode(conteudo)
-
+        texto_sem_pontos_virgulas = conteudo.replace(",", "").replace(";", "").replace(".", "")
+        conteudo_limpo = unidecode(texto_sem_pontos_virgulas)
+        print("Texto Limpo: ")
+        print()
         print(conteudo_limpo)
         
         texto_limpo = True
 
-    elif pergunta_p == '2':
+    elif (pergunta_p == '3' or pergunta_p == '4' or pergunta_p == '5') and not funcao:
+
+        input("Não é possivel escolher essa opção agora, tente outra")
+
+
+    elif (pergunta_p == '2'):
 
         palavras = conteudo_limpo.split()
 
-        #palavrass = []
-        #quantidade = []
-
-        # Conta a frequência de cada palavra usando um dicionário
         frequencia = {}
         for palavra in palavras:
             if palavra in frequencia:
@@ -63,23 +66,23 @@ while pergunta_global != '0':
             else:
                 frequencia[palavra] = 1
 
-        # Ordena as palavras por frequência em ordem decrescente
         palavras_ordenadas = sorted(frequencia, key=frequencia.get, reverse=True)
 
-        # Imprime o resultado
         print("O texto tem", len(palavras), "palavras.")
         print("Palavras por frequência:")
         for palavra in palavras_ordenadas:
             print(palavra, ":", frequencia[palavra])
             palavrass.append(palavra)
             quantidade.append(frequencia[palavra])
-            
+        
+        funcao = True
         #print(palavrass)
         #print(quantidade)
     
     elif pergunta_p == '3':
 
         while True:
+            print()
             pergunta = input("Qual palavra você deseja saber a quantidade de vezes que aparece no texto: ")
             
             if not pergunta:
@@ -87,46 +90,54 @@ while pergunta_global != '0':
                 break
 
             if pergunta in palavrass:
+                
                 print(f"A palavra {pergunta} apareceu {quantidade[palavrass.index(pergunta)]} vezes no texto")
             else:
                 input(f"A palavra {pergunta} não está no texto, digite outra palavra")
 
-    #numeros = []
-
-    #lista_menores_valores = []
-    #lista_maiores_valores = []
 
     elif pergunta_p == '4':
 
         numero_menos_frequente = min(quantidade)
         
-
         for i, numero in enumerate(quantidade):
             if numero == numero_menos_frequente:
                 numeros.append(i)
 
-        ''''''
+        print(numeros)
 
         for x in numeros:
-            lista_menores_valores.append(palavrass[x])
+           lista_menores_valores.append(palavrass[x])
 
+        print()
         print("Lista das palavras que menos aparecem: ")
+        print()
         print(lista_menores_valores)
 
 
     elif pergunta_p == '5':
         numero_mais_frequente = max(quantidade)
 
-        for i, numeross in enumerate(quantidade):
-            if numeross == numero_mais_frequente:
-                numeros.append(i)
+        for en, number in enumerate(quantidade):
+            if number == numero_mais_frequente:
+                numeros1.append(en)
+        
+        for y in numeros1:
+            lista_maiores_valores.append(palavrass[y])
 
+        print()
         print("Lista das palavras que mais aparecem: ")
+        print()
         print(lista_maiores_valores)
 
+
     elif pergunta_p == '0':
+        print()
+        print("saindo do programa...............")
+        print()
         pergunta_global = '0'
     
     else:
+        print()
         input("Esse não é um numero disponível, tente outro... ")
-
+        print()
